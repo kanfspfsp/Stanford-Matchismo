f178d97199ced44507d954cb381df01a35dc62be
karan <kanfspfsp@karans-MacBook-Air.local>
2014-12-16 21:01:19 +0530

    Initial Commit

---
+++ PlayingCardMatching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayingCardMatching
+//
+//  Created by karan on 16/12/14.
+//  Copyright (c) 2014 Karan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayingCardMatching/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayingCardMatching
+//
+//  Created by karan on 16/12/14.
+//  Copyright (c) 2014 Karan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
